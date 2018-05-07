cf91122135a5e54781cbd809fa6833998d4bee3a
dmitriyace <chistohodovda@gmail.com>
2018-05-07 22:13:46 +0300

    Hero_pj

---
+++ src/Enums/ColorsEnum.java
@@ -0,0 +1,5 @@
+package Enums;
+
+public enum ColorsEnum {
+    RED, BLUE, WHITE, GREY
+}
---
+++ src/Hero_Pj.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Hero_Pj {
+    protected Heroes heroes;
+    protected Pj pijama;
+
+
+
+    Hero_Pj(Heroes heroes, Pj pijama){
+        this.heroes = heroes;
+        this.pijama = pijama;
+
+    }
+
+   public static ArrayList<Hero_Pj> h_p = new ArrayList<>();
+
+}
