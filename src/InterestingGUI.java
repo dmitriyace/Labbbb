71cdc15687723437c04e3826d19dac152de33a30
dmitriyace <chistohodovda@gmail.com>
2018-06-05 23:58:15 +0300

    осталась анимация и коннект с сервером

--- src/InterestingGUI.java
+++ src/InterestingGUI.java
@@ -1,5 +1,6 @@
 import Enums.ColorsEnum;
 import Enums.EPj;
+import Enums.EPjc;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
@@ -7,6 +8,7 @@ import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,14 +16,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 public class InterestingGUI {
-    //    public boolean alreadyReleased = false;
-//    int kostyl = 0;
     CopyOnWriteArrayList<Pj> collection = new CopyOnWriteArrayList<>();
     String colorCheckBox = "blueredwhitegrey";
-    //    boolean gotIt;
     boolean ifSizeCorrect = false;
     EPj saveCorrectSize;
     ArrayList<Pj> paintCollection;
+    JSpinner spinClearance;
 
     public static void main(String[] args) {
         new InterestingGUI().go();
@@ -35,28 +35,14 @@ public class InterestingGUI {
         frame.setContentPane(panel);
         frame.setSize(800, 800);
         panel.setLocation(0, 0);
-        //создал объект класса PBtn
+
+        //создаю объект класса PBtn
         PBtn btn = new PBtn();
 
         File file = new File(".\\formtest.xml");
         String path = file.getAbsolutePath();
         In.getPjeys(path, collection);
-//        collection.forEach(n -> {
-//            btn.addBtn(n.loca.getX(), n.loca.getY(), getSizeFromEnum(n.epj), (int) 1.3 * getSizeFromEnum(n.epj), n.name, getColorFromEnum(n.color));
-//        });
 
-//        java.util.List<PBtn> list = new ArrayList<>();
-//        for (Pj pj : collection) {
-//            x = pj.loca.getX();
-//            y = pj.loca.getY();
-//            name = pj.name;
-//            color = getColorFromEnum(pj.color);
-//            width = getSizeFromEnum(pj.epj);
-//            height = (int) 1.8 * width;
-//            list.add(new PBtn(x, y, width, height, name, color));
-//
-//        }
-//        btn.clearBtns();
 
         System.out.println("создаю панель объектов");
         //создал панель объектов
@@ -79,14 +65,7 @@ public class InterestingGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 btn.clearBtns();
-//                collection.clear();
-//                if (!alreadyReleased) {
-//                    if (kostyl!=3){
 
-//                File f = new File(".\\formtest.xml");
-//                String path = f.getAbsolutePath();
-//                In.getPjeys(path, collection);
-                // фильтр по цвету
             }
         });
 
@@ -108,31 +87,21 @@ public class InterestingGUI {
                         }
                     });
                 });
-//                filteredColors.forEach(cl->{
-//                    paintCollection.stream().filter(n->{
-//                     if (n.color.compareTo(cl)==0) return true;
-//                     else return false;
-//                    });
-//                });
-
-//                collection = collection.stream().filter(n -> {
-//
-//                    if (n.color.compareTo(ColorsEnum.WHITE) != 0) return true;
-//                    else return false;
-//                }).collect(Collectors.toCollection(CopyOnWriteArrayList<Pj>::new));
-
-//                collection.forEach(n -> {
-//                    btn.addBtn(n.loca.getX(), n.loca.getY(), getSizeFromEnum(n.epj), (int) 1.3 * getSizeFromEnum(n.epj), n.name, getColorFromEnum(n.color));
-//                });
+                //по размеру
                 if (ifSizeCorrect) {
                     paintCollection = paintCollection.stream().filter(n -> {
                         if (n.getSize().compareTo(saveCorrectSize) == 0) return true;
                         return false;
-//                                System.out.println(n.name);
-//                                return true;
                     }).collect(Collectors.toCollection(ArrayList::new));
                 }
-
+                //по чистоте
+                if (spinnerSetted()){
+                    paintCollection = paintCollection.stream().filter(n->{
+                        if (n.getClearance().compareTo(EPjc.valueOf(spinClearance.getValue().toString().toUpperCase()))==0)
+                            return true;
+                        return false;
+                    }).collect(Collectors.toCollection(ArrayList::new));
+                }
 
                 paintCollection.forEach(n -> {
                     btn.addBtn(n.loca.getX(), n.loca.getY(), getSizeFromEnum(n.epj), (int) 1.3 * getSizeFromEnum(n.epj), n.name, getColorFromEnum(n.color));
@@ -170,6 +139,7 @@ public class InterestingGUI {
         //текстфилд
         JTextField sizeField = new JTextField();
         JLabel checkTextField = new JLabel();
+        checkTextField.setFont(new Font("Consolas",Font.PLAIN,10));
         sizeField.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
@@ -197,12 +167,24 @@ public class InterestingGUI {
         menuPanel.add(checkTextField);
 
         //спиннер
-        String[] clearance = {"washed", "unwashed", "none"};
+        String[] clearance = {"washed", "unwashed", "doesn't matter"};
         SpinnerListModel model = new SpinnerListModel(clearance);
-        JSpinner spinClearance = new JSpinner(model);
+        spinClearance = new JSpinner(model);
         menuPanel.add(spinClearance);
 
+
+
         frame.setVisible(true);
+    }
+
+
+
+    public boolean spinnerSetted(){
+        String value = spinClearance.getValue().toString();
+        if (value==("washed")||value=="unwashed"){
+            return true;
+        }
+        return false;
     }
 
 
@@ -280,24 +262,7 @@ public class InterestingGUI {
 class PBtn extends JComponent {
     private LinkedList<MyBtn> myBtns = new LinkedList<>();
 
-    //    static class MyBtn {
-//        int x;
-//        int y;
-//        int width;
-//        int height;
-//        String name;
-//        Color color;
-//
-//        MyBtn(int x, int y, int width, int height, String name, Color color) {
-//            this.x = x;
-//            this.y = y;
-//            this.color = color;
-//            this.height = height;
-//            this.width = width;
-//            this.name = name;
-//        }
-//
-//    }
+
     static class MyBtn extends Rectangle {
         int x;
         int y;
@@ -330,12 +295,6 @@ class PBtn extends JComponent {
                 ToolTipManager.sharedInstance().mouseMoved(e);
             }
         });
-//        addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseEntered(MouseEvent e) {
-//                setToolTipText(name);
-//            }
-//        });
         myBtns.add(myBtn);
         repaint();
     }
@@ -350,8 +309,6 @@ class PBtn extends JComponent {
         super.paintComponent(g);
         Graphics2D graphics2D = (Graphics2D) g;
         myBtns.forEach(n -> {
-//            graphics2D.setColor(n.color);
-//            graphics2D.fillRect(n.x, n.y, n.width, n.height);
             graphics2D.setColor(n.color);
             graphics2D.draw(n);
             graphics2D.fill(n);
